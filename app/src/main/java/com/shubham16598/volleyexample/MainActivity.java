62dbf0ef67bd0a179f06aed763c8634485d9e1d6
Shubham16598 <shubham16598@gmail.com>
2017-09-02 23:19:28 +0530

    First volley Request

--- app/src/main/java/com/shubham16598/volleyexample/MainActivity.java
+++ app/src/main/java/com/shubham16598/volleyexample/MainActivity.java
@@ -2,12 +2,51 @@ package com.shubham16598.volleyexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView textView;
+    Button button;
+    String server_url = "http://192.168.43.191/webapp/init.php";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView)findViewById(R.id.textView);
+        button = (Button)findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
+
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, server_url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                textView.setText(response);
+                                requestQueue.stop();
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        textView.setText("Error Occured");
+                        requestQueue.stop();
+                    }
+                });
+
+                requestQueue.add(stringRequest);
+            }
+        });
     }
 }
