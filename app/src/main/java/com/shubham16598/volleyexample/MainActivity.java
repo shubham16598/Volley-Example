60af7191590183c959f3ab1e18e1a5f674a606ae
Shubham16598 <shubham16598@gmail.com>
2017-09-03 08:24:11 +0530

    Request Queue optimised to use Cache

--- app/src/main/java/com/shubham16598/volleyexample/MainActivity.java
+++ app/src/main/java/com/shubham16598/volleyexample/MainActivity.java
@@ -6,10 +6,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.android.volley.Cache;
+import com.android.volley.Network;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
@@ -25,10 +30,16 @@ public class MainActivity extends AppCompatActivity {
         textView = (TextView)findViewById(R.id.textView);
         button = (Button)findViewById(R.id.button);
 
+        Cache cache = new DiskBasedCache(getCacheDir(),1024*1024);
+        Network network = new BasicNetwork(new HurlStack());
+
+        final RequestQueue requestQueue = new RequestQueue(cache,network);
+        requestQueue.start();
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                final RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
+                //final RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
 
                 StringRequest stringRequest = new StringRequest(Request.Method.POST, server_url,
                         new Response.Listener<String>() {
