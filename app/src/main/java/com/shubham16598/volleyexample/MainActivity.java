bf26335457790e24c7fdcc55ad97a1289db369b1
Shubham16598 <shubham16598@gmail.com>
2017-09-03 09:46:36 +0530

    ImageRequest Done

--- app/src/main/java/com/shubham16598/volleyexample/MainActivity.java
+++ app/src/main/java/com/shubham16598/volleyexample/MainActivity.java
@@ -1,10 +1,13 @@
 package com.shubham16598.volleyexample;
 
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Cache;
 import com.android.volley.Network;
@@ -15,13 +18,16 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.BasicNetwork;
 import com.android.volley.toolbox.DiskBasedCache;
 import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.ImageRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
     TextView textView;
-    Button button;
+    ImageView imageView;
+    Button button,button2;
     String server_url = "http://192.168.43.191/webapp/init.php";
+    String img_url = "http://192.168.43.191/webapp/photo.jpg";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +35,8 @@ public class MainActivity extends AppCompatActivity {
 
         textView = (TextView)findViewById(R.id.textView);
         button = (Button)findViewById(R.id.button);
+        button2 = (Button)findViewById(R.id.button2);
+        imageView = (ImageView)findViewById(R.id.img);
         //without singleton class method
         /* (2.) Cache cache = new DiskBasedCache(getCacheDir(),1024*1024);
         Network network = new BasicNetwork(new HurlStack());
@@ -60,6 +68,30 @@ public class MainActivity extends AppCompatActivity {
                 //requestQueue.add(stringRequest);
 
                 MySingleton.getInstance(getApplicationContext()).addToRequest(stringRequest);
+
+            }
+        });
+
+
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                final ImageRequest imageRequest = new ImageRequest(img_url,
+                        new Response.Listener<Bitmap>() {
+                            @Override
+                            public void onResponse(Bitmap response) {
+                                imageView.setImageBitmap(response);
+                            }
+                        }, 0, 0, ImageView.ScaleType.CENTER_CROP, null, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this,"Something Went Wrong",Toast.LENGTH_LONG).show();
+                    }
+                });
+
+                MySingleton.getInstance(getApplicationContext()).addToRequest(imageRequest);
 
             }
         });
